b30148a112cb222e5bb57bd53873f9d037f89a3f
johnnycox07 <johnnycoxd@gmail.com>
2026-07-05 18:12:47 +0500

    feat: add custom exception classes

--- src/domain/exceptions/__init__.py
+++ src/domain/exceptions/__init__.py
@@ -2,3 +2,13 @@ class BatchNotFoundError(Exception):
     def __init__(self, batch_id: int):
         self.batch_id = batch_id
         super().__init__(f"Batch {batch_id} not found")
+
+class ProductNotFoundError(Exception):
+    def __init__(self, unique_code: str):
+        self.product_id = unique_code
+        super().__init__(f"Product {unique_code} not found")
+
+class ProductAlreadyAggregatedError(Exception):
+    def __init__(self, unique_code: str):
+        self.unique_code = unique_code
+        super().__init__(f"Product {unique_code} is already aggregated")
